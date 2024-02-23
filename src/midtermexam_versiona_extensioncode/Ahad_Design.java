20ed25a1cd8805645f4234cbb899021f0215e1a1
Ahad Mazhar <mazhaaha@sheridancollege.ca>
2024-02-23 16:37:20 -0500

    branched

---
+++ src/midtermexam_versiona_extensioncode/Ahad_Design.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author Laundry
+ */
+public class Ahad_Design {
+    //My favorite class is java because it involves more bnack-end coding, which I enjoy more than website design, etc.
+}
